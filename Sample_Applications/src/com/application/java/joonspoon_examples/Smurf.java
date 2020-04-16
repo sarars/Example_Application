741eeaf99e821d3c25299b4bfa82abf5aa1a32fd
Saranya <rssaranya8@gmail.com>
2020-04-16 23:47:19 +0530

    CODE : Add examples of Java7 feature, static

---
+++ Sample_Applications/src/com/application/java/joonspoon_examples/Smurf.java
@@ -0,0 +1,26 @@
+package com.application.java.joonspoon_examples;
+
+public class Smurf {
+	
+	private String name;
+	
+	private static int papaCount;
+	
+	private Smurf(String name) {
+		if ( name.equalsIgnoreCase("papa") ) {
+			papaCount++;
+		}
+		this.name = name;
+	}
+	
+	public static Smurf createSmurf( String name ) {
+		if( name.equalsIgnoreCase("handy")  ) {
+			System.out.println("Creating smurf "+ name + "Smurf");
+		}
+		else if( papaCount < 1 ) {
+			System.out.println("Creating smurf "+ name + "Smurf");
+		}
+		return new Smurf(name);
+	}
+
+}
---
+++ Sample_Applications/src/com/application/java/joonspoon_examples/SmurfFactory.java
@@ -0,0 +1,13 @@
+package com.application.java.joonspoon_examples;
+
+public class SmurfFactory {
+
+	public static void main(String[] args) {
+		Smurf.createSmurf("handy");
+		Smurf.createSmurf("papa");
+		Smurf.createSmurf("papa");
+		Smurf.createSmurf("handy");
+		Smurf.createSmurf("handy");
+	}
+
+}
